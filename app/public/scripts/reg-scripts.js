d29d9aee25f404c09e291562fd035795f8d9cf87
CCInCharge <charles.l.chen.clc@gmail.com>
2017-11-14 19:17:53 -0800

    Add frontend scripts for registration view

--- app/public/scripts/reg-scripts.js
+++ app/public/scripts/reg-scripts.js
@@ -29,6 +29,41 @@ $(document).ready(function(){
     var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
     return re.test(email);
   }
+
+  function validInputs() {
+    if (!$("input#first_name").val().match(/[a-z]/i)) {
+      return false;
+    }
+    if (!$("input#last_name").val().match(/[a-z]/i)) {
+      return false;
+    }
+    if (!validateEmail($("input#email").val())) {
+      return false;
+    }
+    if ($("select#usertype").val() == "0") {
+      return false;
+    }
+    if (!$("input#password").val().match(/[a-z]/i)) {
+      return false;
+    }
+    if ($("input#password_confirmation").val() != $("input#password").val()) {
+      return false;
+    }
+    if ($("select#usertype").val() == "3" && !validateEmail($("input#student_email").val())) {
+      return false;
+    }
+    return true;
+  }
+
+  function onRegister() {
+    if (validInputs()) {
+      console.log("True");
+    }
+    else {
+      console.log("False");
+    }
+  }
   
   $("#usertype").change(onChangeUsertype);
-})
+  $("input#register").click(onRegister);
+});
