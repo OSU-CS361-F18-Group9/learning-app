fb1ff03ec439e47409d3e6d1b9a72798e90b4f72
CCInCharge <charles.l.chen.clc@gmail.com>
2017-11-15 14:50:58 -0800

    Add functionality to insert new user to backend DB

---
+++ app/dbcon.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+require('dotenv').config();
+
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : process.env.DB_HOST,
+  user            : process.env.DB_USER,
+  password        : process.env.DB_PW,
+  database        : process.env.DB_USER
+});
+
+module.exports.pool = pool;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,4 @@
+var mysql = require('../dbcon.js');
 var express = require('express');
 var router = express.Router();
 
@@ -10,5 +11,35 @@ router.get('/register', function(req, res, next) {
   res.render('register');
 });
 
+router.post('/register_new_user', function(req,res,next) {
+  mysql.pool.query("SELECT * FROM users WHERE email=?",
+                  [req.body.email],
+                  function (error, result) {
+    if(error){
+      next(error);
+      return;
+    }
+    
+    if (result.length > 0) {
+      // E-mail address must be unique - do not allow registration if
+      // e-mail is already in the database
+      next(error);
+      return;
+    }
+
+    mysql.pool.query("INSERT INTO users (`first_name`, `last_name`, `email`, `password`, `type`) \
+                      VALUES (?, ?, ?, ?, ?)",
+                      [req.body.first_name, req.body.last_name,
+                       req.body.email, req.body.password, parseInt(req.body.usertype)],
+                       function (error, result) {
+      if(error){
+        next(error);
+        return;
+      }
+      // FIXME: Why doesn't this show the registration page?
+      res.render("regSuccess");
+    });
+  });
+});
 
 module.exports = router;
