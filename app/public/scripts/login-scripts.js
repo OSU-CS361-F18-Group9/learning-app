14ed61f9ed4c9f61ad3f43282feb4cbdb6c1f87d
Erica Lee <ericamllee@gmail.com>
2017-11-16 14:40:09 -0800

    added login script

---
+++ app/public/scripts/login-scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  function validateEmail(email) {
+    var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    return re.test(email);
+  }
+
+  function validInputs() {
+    var errors = "";
+    if (!validateEmail($("input#email").val())) {
+      errors += "<li>Need an e-mail address</li>";
+    }
+    
+    if (!$("input#password").val().match(/[a-z]/i)) {
+      errors += "<li>Need a password</li>";
+    }
+    
+    $("ul#errors").html(errors);
+    if (errors == "") {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  function onLogin() {
+    if (validInputs()) {
+      $.ajax({
+        type: "POST",
+        url: '/login',
+        data: $("#regForm").serialize(),
+      });
+    }
+  }
+  
+  $("input#register").click(onLogin);
+});
