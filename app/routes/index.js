fc5f8813d973ddfe7170e4984e9aadcbf7ea4b6e
Erica Lee <ericamllee@gmail.com>
2017-11-15 20:36:46 -0800

    added login to index.js file

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,5 +10,9 @@ router.get('/register', function(req, res, next) {
   res.render('register');
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
 
 module.exports = router;
