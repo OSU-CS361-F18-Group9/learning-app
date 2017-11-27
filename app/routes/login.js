80aba3b4a05a6424b40ccbe9abc99197fc07e0bb
Erica Lee <ericamllee@gmail.com>
2017-11-26 20:05:12 -0800

    sql query is correct now

--- app/routes/login.js
+++ app/routes/login.js
@@ -15,13 +15,13 @@ router.get('/success', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  mysql.pool.query("SELECT first_name, last_name, completion, course_name FROM users u INNER JOIN student_to_course sc ON u.id = sc.sid INNER JOIN courses c ON c.id = sc.cid WHERE email=? AND password=?",
+  mysql.pool.query("SELECT first_name, last_name, completion, course_name FROM users u LEFT OUTER JOIN (SELECT sc.sid, completion, course_name FROM student_to_course sc INNER JOIN courses c ON c.id = sc.cid) as t2 ON t2.sid = u.id WHERE email=? AND password=?",
                       [req.body.email, req.body.password],
                        function (error, result) {
     if (error) {
       next(error);
       return;
-    } else if (result.length != 1) {
+    } else if (result.length < 1) {
       console.log("no one by that login info");
       let ajax = req.xhr;
       if (ajax) {
@@ -33,11 +33,15 @@ router.post('/', function(req, res, next) {
       }
     } else {
       console.log(result);
-      console.log("login successful");
+      // console.log("login successful");
       let ajax = req.xhr;
       if (ajax) {
-        res.json({'msg':'redirect','location':'/login/success'});
-        // res.json({'msg': 'redirect', 'location': '/dashboard', 'fname': result.first_name, 'lname': result.last_name, 'classes': classes});
+        // res.json({'msg':'redirect','location':'/login/success'});
+        res.json({
+          'msg': 'redirect', 
+          'location': '/login/success', 
+          'classes': result
+        });
       }
       else {
         req.method = 'get';
