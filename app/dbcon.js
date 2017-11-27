d28ee9063182c67313fa030e34675fc54555317d
Erica Lee <ericamllee@gmail.com>
2017-11-26 19:17:59 -0800

    fixing the dbcon

--- app/dbcon.js
+++ app/dbcon.js
@@ -3,15 +3,10 @@ require('dotenv').config();
 
 var pool = mysql.createPool({
   connectionLimit : 10,
-  // host            : process.env.DB_HOST,
-  // user            : process.env.DB_USER,
-  // password        : process.env.DB_PW,
-  // database        : process.env.DB_USER
-
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs361_leeerica',
-  password        : 'learning',
-  database        : 'cs361_leeerica'
+  host            : process.env.DB_HOST,
+  user            : process.env.DB_USER,
+  password        : process.env.DB_PW,
+  database        : process.env.DB_USER
 });
 
 module.exports.pool = pool;
